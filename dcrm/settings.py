83b8aeb6668431354c12557aaf396d26bc6eb7e6
sudocoder-py <sudocoder.py@gmail.com>
2025-06-03 11:15:37 +0300

    d23

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -130,11 +130,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS= ['static/']
+# STATICFILES_DIRS= ['static/']
 
 # Add this line:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+if not DEBUG:
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
