447804412f80e0f59c90d9e6d303aac3ad50fb4b
raizeromar <omar.gatara@raizer.tech>
2025-06-04 13:45:28 +0300

    dj import export

--- campaign/resources.py
+++ campaign/resources.py
@@ -5,4 +5,4 @@ class LeadResource(resources.ModelResource):
     class Meta:
         model = Lead
         import_id_fields = ('email',) # Use email as the unique identifier during import
-        fields = ('full_name', 'first_name', 'last_name', 'position', 'email', 'phone_number', 'linkedin_profile', 'company_name', 'company_website', 'industry', 'employee_count', 'campany_linkedin_page', 'location', 'source', 'lead_type', 'created_at') 
+        fields = ('subscribed_company','full_name', 'first_name', 'last_name', 'position', 'email', 'phone_number', 'linkedin_profile', 'company_name', 'company_website', 'industry', 'employee_count', 'campany_linkedin_page', 'location', 'source', 'lead_type', 'created_at') 
