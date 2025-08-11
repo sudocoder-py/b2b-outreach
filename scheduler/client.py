0fa81160f09551160786cc63fd2d4cdf0cfc8c2c
sudocoder-py <sudocoder.py@gmail.com>
2025-08-11 11:18:33 +0000

    making inngest client mode from dev to cloud

--- scheduler/client.py
+++ scheduler/client.py
@@ -1,8 +1,13 @@
 import logging
 import inngest
+from environ import Env
 
-# Create an Inngest client
+env = Env()
+
 inngest_client = inngest.Inngest(
     app_id="social_share",
+    signing_key=env("INNGEST_SIGNING_KEY"),
+    event_key=env("INNGEST_EVENT_KEY"),
     logger=logging.getLogger("gunicorn"),
+    is_production=True,  # Add this to force production mode
 )
