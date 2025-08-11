64280a49962fc55dd7394002cd9caa76850930ef
sudocoder-py <sudocoder.py@gmail.com>
2025-08-11 09:49:11 +0000

    fixing production

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -19,7 +19,7 @@ if os.environ.get("DJANGO_DEBUG", "False") == "True":
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['inngest'])
 
