ddf6e4f57be6cdd9a5cdb0bff1e14fa916505bbe
sudocoder-py <sudocoder.py@gmail.com>
2025-08-13 14:51:13 +0300

    mark as replied v0.3

--- scheduler/client.py
+++ scheduler/client.py
@@ -6,8 +6,5 @@ env = Env()
 
 inngest_client = inngest.Inngest(
     app_id="social_share",
-    signing_key=env("INNGEST_SIGNING_KEY"),
-    event_key=env("INNGEST_EVENT_KEY"),
     logger=logging.getLogger("gunicorn"),
-    is_production=True,  # Add this to force production mode
 )
