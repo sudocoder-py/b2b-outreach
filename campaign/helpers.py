7b92ea28a780274239b885cad74780f1b7cd5fb7
sudocoder-py <sudocoder.py@gmail.com>
2025-08-24 15:11:26 +0300

    added is allwed to launch the campaign and comented planned things

--- campaign/helpers.py
+++ campaign/helpers.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from clients.models import Product
-from campaign.models import Lead, LeadList, Message, Campaign
+from campaign.models import Lead, LeadList, Message, Campaign, MessageAssignment
 from django.core.exceptions import ImproperlyConfigured
 
 def get_subscribed_company(request):
@@ -60,4 +60,77 @@ def get_lead_lists_or_both(request, *, lead_lists_only=False, list_id=None):
     else:
         leads = Lead.objects.filter(subscribed_company=subscribed_company)
         return lead_lists, leads
+
+
+
+
+def is_allowed_to_be_launched(request, pk):
+    """
+    check if the campaign is allowed to be launched
+    """
+    try:
+        campaign = get_object_or_404(Campaign, pk=pk)
+
+        campaign_leads = campaign.campaignlead_set.all()
+        if campaign_leads.count() == 0:
+            return {
+                "allowed": False,
+                "error": "campaign_leads_error",
+                "message": "No campaign leads found for campaign",
+                "campaign_id": pk
+            }
+
+    
+        
+
+        # Check if campaign has message assignments
+        message_assignments_count = MessageAssignment.objects.filter(
+            campaign=campaign,
+            sent=False
+        ).count()
+
+        if message_assignments_count == 0:
+            return {
+                "allowed": False,
+                "error": "message_assignments_error",
+                "message": "No pending message assignments found for campaign",
+                "campaign_id": pk
+            }    
+
+        campaign_options = campaign.campaign_options.first()
+        if not campaign_options:
+            return {
+                "allowed": False,
+                "error": "campaign_options_error",
+                "message": "No options found for campaign",
+                "campaign_id": pk
+            }
+        
+        
+        # # Check email accounts
+        active_email_accounts = campaign_options.email_accounts.filter(status='active')
+        if not active_email_accounts.exists():
+            return {
+                "allowed": False,
+                "error": "email_accounts_error",
+                "message": "No active email accounts found for campaign",
+                "campaign_id": pk
+            }
+        
+
+        return {
+            "allowed": True,
+            "message": "Campaign is allowed to be launched",
+            "campaign_id": pk
+        }
+
+
+    except Exception as e:
+        return {
+            "allowed": False,
+            "error": "unexpected_error",
+            "message": f"Error checking campaign launch status: {str(e)}",
+            "campaign_id": pk
+        }
+
 
--- todo.md
+++ todo.md
@@ -1,24 +1,4 @@
 
-## AI placeholder issue: {leads_company} token not being replaced correctly in emails
+## TODO
 
-## make option where we can make assigment msgs for all the campaign leads and all the campaign messages at once (we can use a for loop maybe?)
-
-## responded content foor the lead?
-
-## change the name of the tracking url to be less susbichous 
-## add a hook for each one, like an email sending to my account using gmail, or notification in the app
-
-
-## make production and staging and dev envs
-
-## finnally make copy the admin css and templates edit them, then use them as frontend! 
-
-## handle reply messages
-
-
-
-
-# aug 7
-
-
-### 4. implemnt the stats for http://127.0.0.1:8000/campaign/messages/ dealyed
+- implemnt campaign action btns; pause, resume
