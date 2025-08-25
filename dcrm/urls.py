b696f8442dfbdbf035791968e6f0001359cf950a
sudocoder-py <sudocoder.py@gmail.com>
2025-08-25 16:53:29 +0300

    renamed /auth/ to /accounts/ : Merge specific files from front1

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -5,25 +5,9 @@ from django.shortcuts import redirect, render
 from scheduler.views import scheduler_inngest_view_path
 from campaign.views import account_settings_view
 from clients.views import home_view
-from django.contrib.auth.decorators import login_not_required
-
 
 def redirect_to_login(request):
     return redirect('login')
-
-
-from django.db import connections
-from django.http import HttpResponse
-from django.db.utils import OperationalError
-
-@login_not_required
-def health_check(request):
-    try:
-        # Check database connectivity
-        connections['default'].cursor()
-        return HttpResponse("healthy", status=200)
-    except OperationalError:
-        return HttpResponse("unhealthy", status=503)
 
 
 urlpatterns = [
@@ -31,12 +15,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('campaign/', include('campaign.urls')),
     path('api/', include('api.urls')),
-    path('auth/', include('clients.urls')),
+    path('accounts/', include('clients.urls')),
     path('support/', include('support.urls')),
     path('feedback/', include('feedback.urls')),
     # path('posts/', include('posts.urls')),
-
-    path('healthy/', health_check, name='health_check'),
 
     path('account-settings/', account_settings_view, name='account_settings'),
 ]
